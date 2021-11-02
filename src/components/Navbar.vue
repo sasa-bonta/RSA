1bc2075f47523d6c44d2197d734ddbead1e852bc
sasa-bonta <bonta2000@mail.ru>
2021-11-02 15:19:03 +0200

    justify center

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@
         color="white"
         flat
     >
-      <v-container class="py-0 fill-height">
+      <v-container class="py-0 fill-height justify-center">
         <v-btn
             v-for="link in links"
             :key="link.route"
@@ -14,18 +14,6 @@
         >
           {{ link.title }}
         </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-              dense
-              flat
-              hide-details
-              rounded
-              solo-inverted
-          ></v-text-field>
-        </v-responsive>
       </v-container>
     </v-app-bar>
   </div>
