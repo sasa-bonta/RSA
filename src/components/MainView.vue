c6a454341caff9609d56473441a1eda7bf4bc412
sasa-bonta <bonta2000@mail.ru>
2021-11-02 20:54:13 +0200

    Compare.vue

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view/>
+  <v-container>
+    <router-view/>
+  </v-container>
 </template>
 
 <script>
--- src/components/pages/Compare.vue
+++ src/components/pages/Compare.vue
@@ -1,10 +1,51 @@
 <template>
-<div>comp</div>
+  <div>
+    <v-textarea
+        label="Fist String"
+        hide-details="auto"
+        filled
+        v-model="field1"
+        height="200"
+    ></v-textarea>
+        <p>Введённое сообщение: {{ field1 }}</p>
+
+    <v-textarea
+        label="Second String"
+        hide-details="auto"
+        filled
+        v-model="field2"
+        height="200"
+    ></v-textarea>
+        <p>Введённое сообщение: {{ field2 }}</p>
+
+    <v-spacer />
+
+    <v-row class="justify-center">
+      <v-btn
+          depressed
+          color="primary"
+          elevation="4"
+          class="mt-8"
+          @click="comp"
+      >
+        Compare
+      </v-btn>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Compare"
+  name: "Compare",
+  data: () => ({
+    field1: '',
+    field2: '',
+  }),
+  methods: {
+    comp() {
+      alert(this.field1 === this.field2)
+    }
+  }
 }
 </script>
 
