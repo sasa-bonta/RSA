25a2db46c313fb027885fe308d3f0152b2b77f11
sasa-bonta <bonta2000@mail.ru>
2021-11-05 17:39:45 +0200

    dist

--- src/store/modules/index.js
+++
@@ -1,5 +0,0 @@
-export {default as keys} from './keys'
-export {default as texts} from './texts'
-export {default as strings} from './strings'
-export {default as result} from './result'
-export {default as vars} from './variables'
